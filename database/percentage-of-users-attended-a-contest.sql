6f769d608c1c3b23f81d123025b49e2c1aaf908c
Y. Huang <105033827+celesteisyy@users.noreply.github.com>
2025-03-03 15:02:12 -0600

    LC-percentage-of-users-attended-a-contest [Runtime: 2024 ms; Memory: 0B]

--- database/percentage-of-users-attended-a-contest.sql
+++ database/percentage-of-users-attended-a-contest.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
-with c as(
-    select contest_id, round((count(user_id)/3)*100 ,2)as percentage
-    from register
-    group by contest_id
-)
-
-select contest_id, percentage
-from c
+select contest_id, round(count(user_id) / (select count(*) from Users) * 100, 2) as percentage
+from Register
+group by contest_id
 order by percentage desc, contest_id asc;
