6dcd894aab5895539a81b84b93e31a93ccd8014d
Y. Huang <105033827+celesteisyy@users.noreply.github.com>
2025-03-07 16:02:03 -0600

    LC-number-of-unique-subjects-taught-by-each-teacher [Runtime: 520 ms; Memory: 0B]

--- database/number-of-unique-subjects-taught-by-each-teacher.sql
+++ database/number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,9 +1,4 @@
 # Write your MySQL query statement below
-with a as(
-    select count(distinct(subject_id)) as num_unique_sub, dept_id, teacher_id
-    from Teacher
-    group by dept_id, teacher_id
-)
-select teacher_id, num_unique_sub as cnt
-from a
+select teacher_id, count(distinct subject_id) as cnt
+from Teacher
 group by teacher_id;
