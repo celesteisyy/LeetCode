8d65e159f4f7fc762eb4563d20b2b5dc490ff5d6
Y. Huang <105033827+celesteisyy@users.noreply.github.com>
2025-03-08 15:56:28 -0600

    LC-product-sales-analysis-iii [Runtime: N/A; Memory: N/A]

--- database/product-sales-analysis-iii.sql
+++ database/product-sales-analysis-iii.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select s.product_id, min(s.year) as first_year, s.quantity, s.price
-from Sales s
-left join Product p on p.product_id = s.product_id
-group by p.product_id;
+select product_id, year as first_year, quantity, price
+from (select s.*, row_number() over (partition by product_id order by year) as rn
+        from Sales s
+        ) as t
+where rn = 1 ;
