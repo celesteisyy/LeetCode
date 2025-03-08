e4e3bba55151452f751d4c73bd90bac04b4a25dd
Y. Huang <105033827+celesteisyy@users.noreply.github.com>
2025-03-08 16:00:09 -0600

    LC-product-sales-analysis-iii [Runtime: 1728 ms; Memory: 0B]

--- database/product-sales-analysis-iii.sql
+++ database/product-sales-analysis-iii.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select product_id, year as first_year, quantity, price
-from (select s.*, row_number() over (partition by product_id order by year) as rn
+from (select s.*, rank() over (partition by product_id order by year) as rn
         from Sales s
         ) as t
-where rn = 1 ;
+where rn = 1;
