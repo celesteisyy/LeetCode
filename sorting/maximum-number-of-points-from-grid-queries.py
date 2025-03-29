1107dba7bbfb518666607cc33967efc7eb54a0bd
Y. Huang <105033827+celesteisyy@users.noreply.github.com>
2025-03-28 19:54:48 -0500

    LC-maximum-number-of-points-from-grid-queries [Runtime: N/A; Memory: N/A]

--- sorting/maximum-number-of-points-from-grid-queries.py
+++ sorting/maximum-number-of-points-from-grid-queries.py
@@ -1,28 +1,28 @@
+# Tried BFS method but cooked with 17/21... So... Here's the DFS solution:
 class Solution:
     def maxPoints(self, grid: List[List[int]], queries: List[int]) -> List[int]:
         m, n = len(grid), len(grid[0])
-        result = [0]* len(queries)
-        directions = [(1,0),(-1,0),(0,1),(0,-1)] #down, up, left, right
-        for q_idx, q in enumerate(queries):
+        result = [0] * len(queries)
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
+        for idx, q in enumerate(queries):
+            if grid[0][0] >= q:
+                result[idx] = 0
+                continue
             visited = [[False]*n for _ in range(m)]
+            dfs = [(0,0)]
             visited[0][0] = True
             count = 0
-            bfs = deque([(0,0)])
-            while bfs:
-                current_m, current_n = bfs.popleft()
-                if grid[current_m][current_n] >= q:
+
+            while dfs:
+                i,j = dfs.pop()
+                if grid[i][j] >= q:
                     continue
-                count +=1
+                count += 1
                 for dir_r, dir_c in directions:
-                    new_m,new_n = current_m + dir_r, current_n + dir_c
-                    if (0 <= new_m < m and 0 <= new_n < n and not visited[new_m][new_n] and grid[new_m][new_n] < q):
-                        bfs.append((new_m,new_n))
-                        visited[new_m][new_n] = True
-            result[q_idx] = count
+                    ni, nj = i + dir_r, j + dir_c
+                    if 0 <= ni < m and 0 <= nj < n and not visited[ni][nj] and grid[ni][nj] < q:
+                        visited[ni][nj] = True
+                        dfs.append((ni,nj)) 
+            
+            result[idx] = count
         return result
-
-            
-            
-
-            
-
